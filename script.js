b1303b9aa0246321cdcfd15d03172b7cdcb3fe46
jabir-jabir <elumilmu@gmail.com>
2026-07-22 16:37:15 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,43 @@
 var socket = io();
-
 var form = document.getElementById('form');
 var input = document.getElementById('input');
 var messages = document.getElementById('messages');
 
+// ইউজার নেম সেট করা
+var userName = prompt("আপনার নাম লিখুন:") || "Anonymous";
+
+// পুরনো মেসেজ লোড করা
+socket.on('load messages', function(docs) {
+    docs.forEach(doc => {
+        displayMessage(doc);
+    });
+});
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     if (input.value) {
-        socket.emit('chat message', input.value);
+        var data = { user: userName, message: input.value };
+        socket.emit('chat message', data);
         input.value = '';
     }
 });
 
-socket.on('chat message', function(msg) {
-    var item = document.createElement('li');
-    item.textContent = msg;
-    messages.appendChild(item);
-    window.scrollTo(0, document.body.scrollHeight);
+socket.on('chat message', function(data) {
+    displayMessage(data);
+    messages.scrollTop = messages.scrollHeight;
 });
+
+function displayMessage(data) {
+    var item = document.createElement('li');
+    item.classList.add('message');
+    
+    // মেসেজটি কি আমার নাকি অন্যের তা চেক করা
+    if (data.user === userName) {
+        item.classList.add('my-msg');
+    } else {
+        item.classList.add('other-msg');
+    }
+
+    item.innerHTML = `<span class="user-name">${data.user}</span>${data.message}`;
+    messages.appendChild(item);
+                    }
